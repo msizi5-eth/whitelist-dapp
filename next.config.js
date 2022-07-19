08100c7af0eaaf6324924ac3e7607df6e54b67b7
Msizi <96126086+msizi5-eth@users.noreply.github.com>
2022-07-19 12:21:32 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  reactStrictMode: false,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
